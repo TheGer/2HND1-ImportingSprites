558f93a41a89e34684d7737548b7664171c532e6
Gerry Said <gerrysaid@gmail.com>
2014-02-19 10:17:28 +0100

    Created menu
    Created a menu and added menu sprites

---
+++ Assets/Scripts/playButtonScript.js
@@ -0,0 +1,18 @@
+﻿#pragma strict
+
+function OnMouseDown()
+{
+	//load the game scene
+	Application.LoadLevel(1);
+	
+	Debug.Log("Clicked Play");
+}
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
---
+++ Assets/Scripts/stripedbarController.js
@@ -0,0 +1,30 @@
+﻿#pragma strict
+var barSpeed:int;
+
+
+function Start () {
+	yield playForThirtySeconds();
+	
+}
+
+function OnGUI()
+{
+	//show the score on the top left corner of the screen.
+}
+
+
+
+function playForThirtySeconds ()
+{
+	yield WaitForSeconds(30);
+	//after 30 seconds go back to the menu scene
+	Application.LoadLevel(0);
+}
+
+
+function Update () {
+
+	//control the stripy bar using the arrow keys
+	transform.Translate(Vector3.right * barSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
+
+}
