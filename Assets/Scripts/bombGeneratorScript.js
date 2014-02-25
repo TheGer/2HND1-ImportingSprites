d85ebe0269d68f4b4dcdd79fa4f0405b309fe00b
Gerry Said <gerrysaid@gmail.com>
2014-02-25 15:33:40 +0100

    lesson 1400-1500
    Added support for four types of sphere

--- Assets/Scripts/bombGeneratorScript.js
+++ Assets/Scripts/bombGeneratorScript.js
@@ -2,11 +2,17 @@
 //reference to the bomb prefab.  We will
 //drag the bomb prefab into this slot
 var bombToGenerate1:Rigidbody;
+var bombToGenerate2:Rigidbody;
+var bombToGenerate3:Rigidbody;
+var bombToGenerate4:Rigidbody;
 
 
 
 function makeBomb()
 {
+	//random number to choose bomb
+	var whichbomb:int;
+	
 	//y position
 	var ypos:int;
 	
@@ -19,10 +25,29 @@ function makeBomb()
 	//x position should be random between the left most and the right most
 	xpos = Random.Range(-1.5,1.5);
 	
+	//range from 1-4
+	whichbomb = Random.Range(1,5);
+	
 	//to create 1 bomb
-	Instantiate(bombToGenerate1,Vector3(xpos,ypos,0),Quaternion.identity);
+	if (whichbomb == 1)
+	{
+		Instantiate(bombToGenerate1,Vector3(xpos,ypos,0),Quaternion.identity);
+	}
 	
+	if (whichbomb == 2)
+	{
+		Instantiate(bombToGenerate2,Vector3(xpos,ypos,0),Quaternion.identity);
+	}
 	
+	if (whichbomb == 3)
+	{
+		Instantiate(bombToGenerate3,Vector3(xpos,ypos,0),Quaternion.identity);
+	}
+	
+	if (whichbomb == 4)
+	{
+		Instantiate(bombToGenerate4,Vector3(xpos,ypos,0),Quaternion.identity);
+	}
 }
 
 
---
+++ Assets/Scripts/cherrybombScript.js
@@ -0,0 +1,16 @@
+﻿#pragma strict
+
+function OnBecameInvisible()
+{
+	//delete the cherry if it goes off screen
+	Destroy(this.gameObject);
+}
+
+
+function Start () {
+
+}
+
+function Update () {
+
+}
