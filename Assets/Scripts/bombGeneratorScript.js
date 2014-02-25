28d0bf838e87b8e2e578354ff8ee92f9fad787d3
Gerry Said <gerrysaid@gmail.com>
2014-02-25 10:07:28 +0100

    lesson 0800-1000
    Lesson 0800-1000

---
+++ Assets/Scripts/bombGeneratorScript.js
@@ -0,0 +1,41 @@
+﻿#pragma strict
+//reference to the bomb prefab.  We will
+//drag the bomb prefab into this slot
+var bombToGenerate1:Rigidbody;
+
+
+
+function makeBomb()
+{
+	//y position
+	var ypos:int;
+	
+	//x position
+	var xpos:float;
+	
+	//y position is the top of the screen
+	ypos = 5;
+	
+	//x position should be random between the left most and the right most
+	xpos = Random.Range(-1.5,1.5);
+	
+	//to create 1 bomb
+	Instantiate(bombToGenerate1,Vector3(xpos,ypos,0),Quaternion.identity);
+	
+	
+}
+
+
+
+function Start () {
+	//this will create one bomb
+	//makeBomb();
+	
+	//this will create one bomb after a second and 
+	//a bomb every 0.5 seconds after that
+	InvokeRepeating("makeBomb",1.0,0.5);
+}
+
+function Update () {
+
+}
--- Assets/Scripts/stripedbarController.js
+++ Assets/Scripts/stripedbarController.js
@@ -1,15 +1,39 @@
 ﻿#pragma strict
 var barSpeed:int;
 
+//string
+//int
+//float
+
+var score:int;
+
+function OnTriggerEnter(bomb:Collider)
+{
+	//this is the code that happens when the bomb hits the paddle
+	if (bomb.gameObject.tag == "cherry")
+	{
+		//increment score by 15
+		score = score + 15;
+	
+	}
+	//remove the bomb
+	Destroy(bomb.gameObject);
+
+}
+
+
 
 function Start () {
+	//reset score to 0
+	score = 0;
 	yield playForThirtySeconds();
 	
 }
 
 function OnGUI()
 {
-	//show the score on the top left corner of the screen.
+	//show the score on the top left corner of the screen.	
+	GUI.Label(Rect(5,5,100,20),"Score: "+score);
 }
 
 
@@ -24,7 +48,7 @@ function playForThirtySeconds ()
 
 function Update () {
 
-	//control the stripy bar using the arrow keys
-	transform.Translate(Vector3.right * barSpeed * Input.GetAxis("Horizontal") * Time.deltaTime);
-
+	//control the stripy bar using the mouse
+	transform.position.x = Camera.main.ScreenToWorldPoint(Input.mousePosition).x;
+	
 }
